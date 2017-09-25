4bc3924a8ee52d11c30938a36a0b92d706a199d9
Hudson Henrique Lopes <hudsonhenriquelopes@gmail.com>
2017-09-25 01:23:57 -0300

    - Será necessário que o aplicativo externo, envie todas as Positions ao longo da trajetória (entre o ponto de início e fim).  - Criação da classe temporária Metodos, para fácil visualização dos métodos que estão sendo manipulados.  - Javadoc dos métodos.

--- app/src/main/java/com/example/hudso/racl/MapsActivity.java
+++ app/src/main/java/com/example/hudso/racl/MapsActivity.java
@@ -1,29 +1,36 @@
 package com.example.hudso.racl;
 
-import android.support.v4.app.FragmentActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import org.json.JSONArray;
 
-    private GoogleMap mMap;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
@@ -39,81 +46,130 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        map = googleMap;
 
 //        // TODO Hudson - Adicionar marcador em Sydney, mapa simples (plano)
 //        addSydneyMarkerOnMap();
 
 //        // TODO Hudson - Adicionar marcador na Estátua da Liberdade, mapa customizado
 //        addStatueOfLibertyMarkerOnMap();
-        CameraPosition liberty =
-                CameraPosition.builder().target(new LatLng(40.689247, -74.044502))
-                        .zoom(20)
-                        .bearing(8)
-                        .tilt(45)
-                        .build();
+//
+        map.setOnMapClickListener(
+                new GoogleMap.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(LatLng latLng) {
+                        MapsActivity.this.onMapClick(latLng);
+                    }
+                }
+        );
 
-        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(liberty));
+        drawLinesRoute();
+
+//        Metodos.moveToMarkerOnSimpleView(map, new LatLng(-26.88665791301889, -49.09618478268385));
+//        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+//        map.setMinZoomPreference(5);
+//        map.getUiSettings().setAllGesturesEnabled(true);
+//        map.setBuildingsEnabled(true);
+//
+//        CameraPosition liberty =
+//                CameraPosition.builder().target(new LatLng(-26.88665791301889, -49.09618478268385))
+//                        // Zoom do mapa
+//                        .zoom(16)
+//                        //
+//                        .bearing(8)
+//                        // Inclinação
+//                        .tilt(45)
+//                        .build();
+//
+//        map.moveCamera(CameraUpdateFactory.newCameraPosition(liberty));
 
     }
 
-    /**
-     * Adicionar marcador na Estátua da Liberdade no mapa.
-     */
-    private void addStatueOfLibertyMarkerOnMap() {
-        MapsInitializer.initialize(getBaseContext());
+    public void drawDynamicRoute() {
+        /**
+        ArrayList<LatLng> markerPoints = new ArrayList<LatLng>();
+        markerPoints.add(point);
 
-        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        // Creating MarkerOptions
+        MarkerOptions options = new MarkerOptions();
 
+        // Setting the position of the marker
+        options.position(point);
+         **/
+    }
 
-        LatLng latLngStatue = new LatLng(40.689247, -74.044502);
+    public void drawLinesRoute() {
+        List<LatLng> decodedPath = new ArrayList<>();
+
+//        decodedPath.add(new LatLng(-26.88665791301889, -49.09618478268385));
+//        decodedPath.add(new LatLng(-26.886526637132196, -49.09517459571362));
+//        decodedPath.add(new LatLng(-26.886891159092812, -49.09469179809094));
+//        decodedPath.add(new LatLng(-26.883331711814563, -49.0956449881196));
+//        decodedPath.add(new LatLng(-26.88331705872923, -49.09618478268385));
+//        decodedPath.add(new LatLng(-26.884531763149415, -49.0958783403039));
+//        decodedPath.add(new LatLng(-26.88361610091067, -49.09616868942977));
+//        decodedPath.add(new LatLng(-26.885169911348147, -49.09624245017767));
+//        decodedPath.add(new LatLng(-26.886304155554605, -49.09699078649282));
+//        decodedPath.add(new LatLng(-26.885434559380023, -49.095554798841476));
+//        decodedPath.add(new LatLng(-26.886654324613982, -49.09627664834262));
+
+        decodedPath.add(new LatLng(-26.90029, -49.08502));
+        decodedPath.add(new LatLng(-26.90015,-49.08482));
+        decodedPath.add(new LatLng(-26.8999,-49.08452));
+        decodedPath.add(new LatLng(-26.8997,-49.08425));
+        decodedPath.add(new LatLng(-26.89951,-49.08399));
+        decodedPath.add(new LatLng(-26.89951,-49.08399));
+        decodedPath.add(new LatLng(-26.89923,-49.08422));
+        decodedPath.add(new LatLng(-26.89916,-49.08427));
+        decodedPath.add(new LatLng(-26.89893,-49.08439));
+        decodedPath.add(new LatLng(-26.89873,-49.08451));
+        decodedPath.add(new LatLng(-26.89867,-49.08453));
+        decodedPath.add(new LatLng(-26.89867,-49.08453));
+        decodedPath.add(new LatLng(-26.8989,-49.08484));
+
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
         MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(latLngStatue);
-        // Título do marcador
-        markerOptions.title("Statue of liberty");
-        // Descrição complementar do marcador
-        markerOptions.snippet("I hope to go there");
+        markerOptions.position(decodedPath.get(0));
 
         // Adicionar o marcador ao mapa
-        mMap.addMarker(markerOptions);
+        map.addMarker(markerOptions);
+        map.addMarker(markerOptions);
 
-        moveToMarkerOnCustomView(latLngStatue);
-    }
+        map.addPolyline(
+                new PolylineOptions()
+                        .addAll(decodedPath)
+                        .color(Color.RED)
+                        .width(10)
+//                        .geodesic(true)
+        );
 
-    /**
-     * Adicionar marcador em Sydney no mapa.
-     */
-    private void addSydneyMarkerOnMap() {
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-
-        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-
-        moveToMarkerOnSimpleView(sydney);
-    }
-
-    /**
-     * Mover a posição do mapa, para a view simples (plana) na posição indicada.
-     * @param position
-     */
-    private void moveToMarkerOnSimpleView(LatLng position) {
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
-    }
-
-    /**
-     * Mover a posição do mapa, para view customizada na posição indicada.
-     * @param position
-     */
-    private void moveToMarkerOnCustomView(LatLng position) {
         CameraPosition liberty =
-                CameraPosition.builder().target(position)
+                CameraPosition.builder().target(decodedPath.get(0))
+                        // Zoom do mapa
                         .zoom(16)
+                        //
                         .bearing(8)
+                        // Inclinação
                         .tilt(45)
                         .build();
 
-        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(liberty));
+        map.moveCamera(CameraUpdateFactory.newCameraPosition(liberty));
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        String text = "decodedPath.add(new LatLng(" + latLng.latitude + ", " + String.valueOf(latLng.longitude) + "));";
+        Toast.makeText(getBaseContext(), text, Toast.LENGTH_LONG);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+
+        // Adicionar o marcador ao mapa
+        map.addMarker(markerOptions);
+
+        drawLinesRoute();
+
+        Log.println(Log.WARN, "Click ", text);
     }
 }
---
+++ app/src/main/java/com/example/hudso/racl/Metodos.java
@@ -0,0 +1,88 @@
+package com.example.hudso.racl;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * Created by hudso on 24/09/2017.
+ */
+
+public class Metodos {
+
+    public static LatLng getStatueOfLibertyPosition() {
+        return new LatLng(40.689247, -74.044502);
+    }
+
+    public static LatLng getSydneyPosition() {
+        return new LatLng(-34, 151);
+    }
+
+    /**
+     * Adicionar marcador na Estátua da Liberdade no mapa.
+     */
+    public static void addStatueOfLibertyMarkerOnMap(GoogleMap map) {
+//        // TODO Hudson - Descobrir por quê existe esta linha de comando
+//        MapsInitializer.initialize(getBaseContext());
+
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(getStatueOfLibertyPosition());
+        // Título do marcador
+        markerOptions.title("Statue of liberty");
+        // Descrição complementar do marcador
+        markerOptions.snippet("I hope to go there");
+
+        // Adicionar o marcador ao mapa
+        map.addMarker(markerOptions);
+
+        moveToMarkerOnCustomView(map, markerOptions.getPosition());
+    }
+
+    /**
+     * Adicionar marcador em Sydney no mapa.
+     */
+    public static void addSydneyMarkerOnMap(GoogleMap map) {
+        // Add a marker in Sydney and move the camera
+        map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+
+        MarkerOptions markerOptions = new MarkerOptions()
+                .position(getSydneyPosition())
+                .title("Marker in Sydney");
+
+        map.addMarker(markerOptions);
+
+        moveToMarkerOnSimpleView(map, markerOptions.getPosition());
+    }
+
+    /**
+     * Mover a posição do mapa, para a view simples (plana) na posição indicada.
+     * @param position
+     */
+    public static void moveToMarkerOnSimpleView(GoogleMap map, LatLng position) {
+        map.moveCamera(CameraUpdateFactory.newLatLng(position));
+    }
+
+    /**
+     * Mover a posição do mapa, para view customizada na posição indicada.
+     * @param position
+     */
+    public static void moveToMarkerOnCustomView(GoogleMap map, LatLng position) {
+        // Customizar posição
+        CameraPosition liberty =
+                CameraPosition.builder().target(position)
+                        // Zoom do mapa
+                        .zoom(16)
+                        //
+                        .bearing(8)
+                        // Inclinação
+                        .tilt(45)
+                        .build();
+
+        map.moveCamera(CameraUpdateFactory.newCameraPosition(liberty));
+    }
+}
